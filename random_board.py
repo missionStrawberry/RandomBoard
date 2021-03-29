759c8bf1e72827a6b1763e6e7e5c1adac46c5c90
missionStrawberry <annam7245@sd61learn.ca>
2021-03-29 11:28:33 -0700

    add code

--- random_board.py
+++ random_board.py
@@ -1,2 +1,13 @@
 arr = [[0 for i in range(10)] for j in range(10)]
-print(arr)
+
+for i in arr:
+    for j in arr[i]:
+        j = int(random() * 9 + 1)
+        
+print2DArray()
+        
+        
+def print2DArray():
+    for i in arr:
+        for j in arr[i]:
+            print(j)
