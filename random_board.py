66807a5f78a42678b33c11699e2281b5f4f296db
missionStrawberry <annam7245@sd61learn.ca>
2021-03-29 11:38:47 -0700

    more progress

--- random_board.py
+++ random_board.py
@@ -1,13 +1,15 @@
+import random
+
+def print2DArray():
+    for i in arr:
+        print(i)
+
 arr = [[0 for i in range(10)] for j in range(10)]
 
-for i in arr:
-    for j in arr[i]:
-        j = int(random() * 9 + 1)
+for i in range(10):
+    for j in range(10):
+        arr[i][j] = random.randint(1,9)
         
 print2DArray()
         
         
-def print2DArray():
-    for i in arr:
-        for j in i:
-            print(j)
